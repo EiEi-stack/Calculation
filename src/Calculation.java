19bfdd8626a6f7f223a981cff7e2e7ba295dc63e
EiEi-stack <yccvenusstudent@gmail.com>
2020-09-24 09:40:17 +0900

    再帰的な実装

--- src/Calculation.java
+++ src/Calculation.java
@@ -1,62 +1,92 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Calculation {
     private static String formula;
-    private static String operatorArrRaw[];
-    private static String operandArrRaw[];
 
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         try {
-
+            //[+-]?([0-9]*[.])?[0-9]+])
             Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter Formula:");
-            formula = scanner.nextLine().trim();
-            int rec = calc(formula);
-            System.out.println("Result is:" + rec);
+            System.out.print("INPUT:");
+            formula = scanner.nextLine();
             scanner.close();
         } catch (InputMismatchException e) {
             System.out.println(e);
 
-        }catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             System.out.println(e);
         }
-    }
 
-    public static int calc(String sFormula) {
-//		BigDecimal result = new BigDecimal(0);
-        int result = 0;
-        // 実装start
-        int aggregate = 0;
-        operatorArrRaw = sFormula.split("[0-9]+", -1);
-        operandArrRaw = sFormula.split("[()+\\-*/]", -1);
 
-        if (!operandArrRaw[0].isEmpty()) {
-            aggregate = Integer.parseInt(operandArrRaw[0]);
-        }
-        for (int i = 0; i < operatorArrRaw.length; i++) {
+        String[] rawInputArray = formula.split("", -1);
+        List<String> inputArray = new ArrayList<>();
+        String temp = "";
 
-            switch (operatorArrRaw[i]) {
-                case "+":
-                    aggregate += Integer.parseInt(operandArrRaw[i]);
-                    break;
-                case "-":
-                    aggregate -= Integer.parseInt(operandArrRaw[i]);
-                    break;
-                case "*":
-                    aggregate *= Integer.parseInt(operandArrRaw[i]);
-                    break;
-                case "/":
-                    aggregate /= Integer.parseInt(operandArrRaw[i]);
-                    break;
-                default:
-                    break;
+
+        for (String value : rawInputArray) {
+            if (value.matches("^[0-9]+|[.]|[0-9]+")) {
+                temp += value;
+                continue;
             }
-
+            if (value.matches("[()+\\-*/]")) {
+                inputArray.add(temp);
+                inputArray.add(value);
+                temp = "";
+                continue;
+            }
         }
+        inputArray.add(temp);
+        inputArray.removeAll(Arrays.asList("", null));
+        String inputString = String.join("", inputArray);
+        //((12.9-80)*70ab/4)
 
-        // 実装end
-        return aggregate;
+        Calculation cal = new Calculation();
+        double result = cal.calc(inputString);
+        System.out.println(result);
     }
+
+    private double calc(String sFormula) {
+//        BigDecimal result = new BigDecimal(0);
+        // 実装start[^([-+/*]\d+(\.\d+)?)*]
+        // 実装end
+        int index;
+        if (sFormula.charAt(0) == '-' || sFormula.charAt(0) == '+') {
+            sFormula = '0' + sFormula;
+        }
+        if ((index = find(sFormula, '+')) >= 0) {
+            return (calc(sFormula.substring(0, index) + calc(sFormula.substring(index + 1, sFormula.length()))));
+        } else if ((index = find(sFormula, '-')) >= 0) {
+            return (calc(sFormula.substring(0, index)) - calc(sFormula.substring(index + 1, sFormula.length())));
+        } else if ((index = find(sFormula, '*')) >= 0) {
+            return (calc(sFormula.substring(0, index)) * calc(sFormula.substring(index + 1, sFormula.length())));
+        } else if ((index = find(sFormula, '/')) >= 0) {
+            return (calc(sFormula.substring(0, index)) / calc(sFormula.substring(index + 1, sFormula.length())));
+        }
+        if (sFormula.charAt(0) == '(') {
+            if (sFormula.charAt(sFormula.length() - 1) == ')') {
+                return (calc(sFormula.substring(1, sFormula.length() - 1)));
+            } else {
+
+            }
+        }
+        return Double.parseDouble(sFormula);
+    }
+
+    private int find(String inpuString, char ch) {
+        int count = 0;
+        for (int i = inpuString.length() - 1; i >= 0; i--) {
+            if (inpuString.charAt(i) == '(') {
+                count++;
+            }
+            if (inpuString.charAt(i) == ')') {
+                count--;
+            }
+            if (inpuString.charAt(i) == ch && count == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
 
 }
